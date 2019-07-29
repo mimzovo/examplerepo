58f1a1f598ae120d914a2864e0bc2b19992d7ace
Michelle Chen <mimzovo@usc-guestwireless-upc-newsc4657.usc.edu>
2019-07-29 10:32:00 -0700

    Added githubdemo.py

---
+++ githubdemo.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------
+#        Name: Michelle Chen
+#    Filename: demo.py
+#        Date: 7/29/19
+#
+# Description: for github
+# -------------------------------------------------
+
+def main():
+    name = "michelle"
+    print("my name is",name)
+
+if __name__ == "__main__":
+    main()
